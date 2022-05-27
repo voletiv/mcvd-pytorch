e91587db76d1da38ed3166e1b4030fb9af06545e
Vikram Voleti <vikram.voleti@gmail.com>
2022-05-27 18:15:18 -0400

    Adds DEMO for SMMNIST

--- load_model_from_ckpt.py
+++ load_model_from_ckpt.py
@@ -5,6 +5,7 @@ import torch
 import yaml
 
 from collections import OrderedDict
+from functools import partial
 from imageio import mimwrite
 from torch.utils.data import DataLoader
 from torchvision.utils import make_grid, save_image
@@ -18,7 +19,7 @@ except:
 
 from datasets import get_dataset, data_transform, inverse_data_transform
 from main import dict2namespace
-from models import get_sigmas, anneal_Langevin_dynamics
+from models import get_sigmas, anneal_Langevin_dynamics, anneal_Langevin_dynamics_consistent, ddpm_sampler, ddim_sampler, FPNDM_sampler
 from models.ema import EMAHelper
 from runners.ncsn_runner import get_model
 
@@ -35,7 +36,7 @@ def parse_args():
 
 
 # Make and load model
-def load_model(ckpt_path, device):
+def load_model(ckpt_path, device=device):
     # Parse config file
     with open(os.path.join(os.path.dirname(ckpt_path), 'config.yml'), 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
@@ -58,6 +59,57 @@ def load_model(ckpt_path, device):
         ema_helper.ema(scorenet)
     scorenet.eval()
     return scorenet, config
+
+
+def get_sampler_from_config(config):
+        version = getattr(config.model, 'version', "DDPM")
+        # Sampler
+        if version == "SMLD":
+            consistent = getattr(config.sampling, 'consistent', False)
+            sampler = anneal_Langevin_dynamics_consistent if consistent else anneal_Langevin_dynamics
+        elif version == "DDPM":
+            sampler = partial(ddpm_sampler, config=config)
+        elif version == "DDIM":
+            sampler = partial(ddim_sampler, config=config)
+        elif version == "FPNDM":
+            sampler = partial(FPNDM_sampler, config=config)
+        return sampler
+
+
+def get_sampler(config):
+    sampler = get_sampler_from_config(config)
+    sampler_partial = partial(sampler, n_steps_each=config.sampling.n_steps_each,
+                    step_lr=config.sampling.step_lr, just_beta=False,
+                    final_only=True, denoise=config.sampling.denoise,
+                    subsample_steps=getattr(config.sampling, 'subsample', None),
+                    clip_before=getattr(config.sampling, 'clip_before', True),
+                    verbose=False, log=False, gamma=getattr(config.model, 'gamma', False))
+    def sampler_fn(init, scorenet, cond, cond_mask, subsample=getattr(config.sampling, 'subsample', None), verbose=False):
+        init = init.to(config.device)
+        cond = cond.to(config.device)
+        if cond_mask is not None:
+            cond_mask = cond_mask.to(config.device)
+        return inverse_data_transform(config, sampler_partial(init, scorenet, cond=cond, cond_mask=cond_mask,
+                                                              subsample_steps=subsample, verbose=verbose)[-1].to('cpu'))
+    return sampler_fn
+
+
+def init_samples(n_init_samples, config):
+    # Initial samples
+    # n_init_samples = min(36, config.training.batch_size)
+    version = getattr(config.model, 'version', "DDPM")
+    init_samples_shape = (n_init_samples, config.data.channels*config.data.num_frames, config.data.image_size, config.data.image_size)
+    if version == "SMLD":
+        init_samples = torch.rand(init_samples_shape)
+        init_samples = data_transform(self.config, init_samples)
+    elif version == "DDPM" or self.version == "DDIM" or self.version == "FPNDM":
+        if getattr(config.model, 'gamma', False):
+            used_k, used_theta = net.k_cum[0], net.theta_t[0]
+            z = Gamma(torch.full(init_samples_shape, used_k), torch.full(init_samples_shape, 1 / used_theta)).sample().to(config.device)
+            init_samples = z - used_k*used_theta # we don't scale here
+        else:
+            init_samples = torch.randn(init_samples_shape)
+    return init_samples
 
 
 if __name__ == '__main__':
